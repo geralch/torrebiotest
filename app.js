33a93c1b3d7ae1f21b993a38879322246dffa89a
Jorge Eduardo Rodriguez <geraldine.caicedo.h@gmail.com>
2018-12-22 05:38:10 -0500

    testass

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
-var express = require('express');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// const cors = require("cors")
+// const bodyParser = require('body-parser');
+// var indexRouter = require('./routes/index'); 
+// var linkedInRouter=require("./routes/linkedIn");
+// var torrenegra=require("./routes/torreNegra");
+
+// var app = express();
+// app.use(cors())
+// app.use(logger('dev'));
+// app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// // parse application/json
+// app.use(bodyParser.json())
+ 
+
+// app.use(cookieParser());
+// //app.use(express.static(path.join(__dirname, 'public')));
+// app.use( (req, res, next)=> {
+//      console.log(req.body) 
+//     next();
+//   }); 
+//   app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+// });
+// app.use('/linkedin', linkedInRouter); 
+// app.use("/torrenegra",torrenegra);
+// module.exports = app; 
+var express = require("express");
+var port = process.env.PORT || 3000;
+var app = express();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require("cors")
-const bodyParser = require('body-parser');
-var indexRouter = require('./routes/index'); 
+const cors = require("cors");
+const bodyParser = require('body-parser'); 
 var linkedInRouter=require("./routes/linkedIn");
 var torrenegra=require("./routes/torreNegra");
-
-var app = express();
-app.use(cors())
-app.use(logger('dev'));
+ app.use(cors())
+ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
 app.use(bodyParser.json())
- 
-
-app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use( (req, res, next)=> {
-     console.log(req.body) 
-    next();
-  }); 
-  app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", function (req, res) {
+ res.send(JSON.stringify({ Hello: ""}));
 });
 app.use('/linkedin', linkedInRouter); 
 app.use("/torrenegra",torrenegra);
-module.exports = app;
+app.listen(port, function () {
+ console.log(`Example app listening on port !`);
+});
